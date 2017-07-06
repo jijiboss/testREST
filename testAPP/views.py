a37864601c2e6170c74f3786a9f3100adf2fe4cb
jijiboss <maroki.sugimoto@outlook.com>
2017-07-06 16:54:28 -0400

    trying to get API view

--- testAPP/views.py
+++ testAPP/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductList(APIView):
+
+    def get(self, request, format=None):
+        prods = Product.objects.all()
+        serializer = ProductSerializer(prods, many=True) #tells there are multiple data
+        return Response(serializer.data)
+
+#    def post(self):

43f42d849ec04fc91e86918f8867e350481f71d0
jijiboss <maroki.sugimoto@outlook.com>
2017-07-06 15:53:08 -0400

    Created model and superuser. about to serialize and view

--- testAPP/models.py
+++ testAPP/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    prod_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+
+    def __str__(self):
+        return '(' + self.prod_id + ')' + self.name + ' - ' + self.description
